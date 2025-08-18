0fb958a1aaf3c7d945eac7bbe526518b6a803ded
GastonFromMetio <gaston@metio.fr>
2025-08-18 13:58:15 +0200

    feat: documentation-personnalisation des couleurs

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -7,6 +7,9 @@
 
     <title>{{ config('app.name', 'TinyTalkAI') }}</title>
 
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('TinyTalkAi_Logo.png') }}" type="image/png">
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,7 @@
 
         <title>{{ config('app.name', 'TinyTalkAI') }}</title>
 
-        <!-- Favicon -->
-        <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml">
-        <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
+        <link rel="icon" href="{{ asset('TinyTalkAi_Logo.png') }}" type="image/png">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
